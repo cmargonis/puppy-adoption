2d0949b650b162382253c3d3d8dcad5eebff2782
Chris Margonis <chris.margonis@withplum.com>
2021-02-27 12:58:24 +0200

    Add temporary puppy list

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,16 +16,37 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,8 +60,46 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val tempPic = "https://images.dog.ceo/breeds/akita/An_Akita_Inu_resting.jpg"
+    val items = listOf(
+        Puppy("Chris", tempPic),
+        Puppy("Rocky", tempPic),
+        Puppy("Rex", tempPic),
+        Puppy("jax", tempPic)
+    )
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyColumn(Modifier.fillMaxWidth()) {
+            items(items) { puppy ->
+                Row {
+                    CoilImage(
+                        data = puppy.profilePicture,
+                        contentDescription = "${puppy.name}'s picture",
+                        modifier = Modifier
+                            .width(64.dp)
+                            .height(64.dp),
+                        contentScale = ContentScale.Fit,
+                        loading = {
+                            Box(Modifier.matchParentSize()) {
+                                CircularProgressIndicator(Modifier.align(Alignment.Center))
+                            }
+                        },
+                        error = {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_error),
+                                colorFilter = ColorFilter.tint(color = colorResource(id = R.color.design_default_color_error)),
+                                modifier = Modifier.size(16.dp),
+                                contentScale = ContentScale.None,
+                                contentDescription = null
+                            )
+                        },
+                        onRequestCompleted = { state ->
+                            Log.i("tag", "Load state: $state")
+                        }
+                    )
+                    Text(text = puppy.name)
+                }
+            }
+        }
     }
 }
 
